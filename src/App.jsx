d591859562daa2b4a0a931c037672ed821c772e1
YMP-BLACRUSH <67401418+YMP-BLACRUSH@users.noreply.github.com>
2022-01-03 15:34:21 +0700

    React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
 import React from 'react'
-// import Cart from './pages/Cart';
-// import Login from './pages/Login';
-// import Product from './pages/Product';
-// import Register from './pages/Register';
+import Cart from './pages/Cart';
+import Login from './pages/Login';
+import Product from './pages/Product';
+import Register from './pages/Register';
 import Home from './pages/Home';
-// import ProductList from './pages/ProductList';
+import ProductList from './pages/ProductList';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
 
 const App = () => {
+  const user = true;
   return (
-    <div>
-      <Home/>
-    </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/products/:category" element={<ProductList/>} />
+          <Route path="/product/:id" element={<Product/>} />
+          <Route path="/cart" element={<Cart/>} />
+          <Route path="/login" element={user ? <Navigate replace to="/"/> : <Login />}/> 
+          <Route path="/register" element={user ? <Navigate replace to="/"/> : <Register />}/> 
+        </Routes>
+      </Router>
   )
 }
 
