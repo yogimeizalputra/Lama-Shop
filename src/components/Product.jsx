58e84201dd4eebedc72378bb5bd8b9882429fe84
YMP-BLACRUSH <67401418+YMP-BLACRUSH@users.noreply.github.com>
2022-01-04 23:01:18 +0700

    Fetch and Filter Products

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { FavoriteBorderOutlined, SearchOutlined, ShoppingCartOutlined } from '@material-ui/icons'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
 const Info = styled.div`
     opacity: 0;
@@ -70,7 +71,9 @@ const Product = ({ item }) => {
                     <ShoppingCartOutlined/>
                 </Icon>
                 <Icon>
-                    <SearchOutlined/>
+                    <Link to={`/product/${item._id}`}>
+                        <SearchOutlined />
+                    </Link>
                 </Icon>
                 <Icon>
                     <FavoriteBorderOutlined/>
